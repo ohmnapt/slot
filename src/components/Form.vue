80c0e33ac67f69a14dc7a6c5ad0cd7e9679b309e
ohmnapt <napats.work@gmail.com>
2021-12-07 02:13:57 +0700

    commit slot

---
+++ src/components/Form.vue
@@ -0,0 +1,14 @@
+<template>
+  <form>
+    <div class="help">
+      <slot name="help"></slot>
+    </div>
+    <div class="field">
+      <slot name="field"></slot>
+    </div>
+    <div class="button">
+      <slot name="button"></slot>
+    </div>
+    <slot></slot>
+  </form>
+</template>
